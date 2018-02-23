14cb567f56e76bfeb42ef1b0fc1c0dd59941a894
hyjcatty <hyjcatty@gmail.com>
2018-02-23 23:07:40 +0800

    remove clearbillboard from workview

--- src/container/workview/workview.js
+++ src/container/workview/workview.js
@@ -95,7 +95,7 @@ export default class workview extends Component {
         if(configuration!==null){
             this.setState({configuration:configuration,status:"run"});
             this.refs.Billboardview.update_configuration(configuration);
-            this.refs.Billboardview.clearbillboard();
+            //this.refs.Billboardview.clearbillboard();
         }else{
             this.setState({status:"run"});
         }
